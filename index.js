898c96d92a93eebb398f1d7cbca3f8608738cc42
tajveez <tajveez786@gmail.com>
2021-07-09 13:10:33 +0500

    Options added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(process.argv);
+const program = require("commander");
+
+program.version("1.0.0").description("Simple password generator");
+
+program
+  .option("-l, --length <number>", "length of password", 8)
+  .option("-s, --save", "save generated password to passwords.txt")
+  .option("-nn, --no-numbers", "Remove numbers from password")
+  .option("-ns, --no-symbols", "Remove symbols from password")
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
